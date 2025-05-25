b6bd48cfc6c4f38cdb3e618867f7cbd12e54ab51
Vlad Lesun <vladlesunmma@gmail.com>
2025-05-25 22:23:34 +0300

    feat: change weblayout for react

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import 'normalize.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import './scss/index.scss';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
---
+++ src/views/Main/Main.jsx
@@ -0,0 +1,17 @@
+import { Filter } from '../../components/Filter/Filter';
+import { Goods } from '../../components/Goods/Goods';
+import { Hero } from '../../components/Hero/Hero';
+import { Subscribe } from '../../components/Subscribe/Subscribe';
+
+const Main = () => {
+	return (
+		<main>
+			<Hero />
+			<Filter />
+			<Goods />
+			<Subscribe />
+		</main>
+	);
+};
+
+export default Main;
